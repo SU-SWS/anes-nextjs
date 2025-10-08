5fbc58f23f65a02dbdf5402fce09023b22f5ec12
rebeccahongsf <rebeccahongsf@gmail.com>
2025-10-07 17:27:13 -0700

    cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import GlobalPage from "@components/layouts/global-page"
 import {getHomePagePath} from "@lib/gql/gql-queries"
 import {Lato, Merriweather, Source_Sans_3, Source_Serif_4} from "next/font/google"
 import localFont from "next/font/local"
-// import {getRandomGradient} from "@lib/utils/get-random-gradient"
+import {getRandomGradient} from "@lib/utils/get-random-gradient"
 
 const appleIcons: Icon[] = [60, 72, 76, 114, 120, 144, 152, 180].map(size => ({
   url: `https://www-media.stanford.edu/assets/favicon/apple-touch-icon-${size}x${size}.png`,
@@ -72,8 +72,7 @@ export const metadata = {
 
 const RootLayout = async ({children, modal}: {children: React.ReactNode; modal: React.ReactNode}) => {
   const homePath = await getHomePagePath()
-  // @TODO: Uncomment before merge
-  // const gradient = getRandomGradient()
+  const gradient = getRandomGradient()
   return (
     <html
       lang="en"
@@ -87,9 +86,7 @@ const RootLayout = async ({children, modal}: {children: React.ReactNode; modal: 
     >
       <UserAnalytics />
       <DrupalWindowSync homePath={homePath} />
-      <body
-      // className={gradient}
-      >
+      <body className={gradient}>
         <nav aria-label="Skip Links">
           <a href="#main-content" className="skiplink">
             Skip to main content
