485b68b12075e3da7663db842e359cd2f303a44f
Rebecca Hong <34019925+rebeccahongsf@users.noreply.github.com>
2025-10-09 13:28:20 -0700

    ANES3-381 ANES3-385 | add motion package and supporting element components (#14)
    * ANES3-381 ANES3-385 | add motion package and supporting element components
    
    * add example to hero banner
    
    * rename files
    
    * cleanup unnecessary ts
    
    * revert
    
    ---------
    
    Co-authored-by: Mike Decker <pookmish@stanford.edu>

---
+++ src/components/elements/animate/animate-parallax.tsx
@@ -0,0 +1,36 @@
+"use client"
+import {useRef, ReactNode} from "react"
+import {motion, useScroll, useTransform, useSpring, useReducedMotion} from "motion/react"
+
+type AnimateParallaxProps = {
+  /**
+   * This is the vertical offset in px that the element will move in response to the scroll.
+   * The larger the offset, the more apparent the parallax effect.
+   * For a realistic effect, use a lower value for the background layer and a higher value for the foreground element.
+   */
+  offset?: number
+  children: ReactNode
+}
+
+const AnimateParallax = ({children, offset = 60}: AnimateParallaxProps) => {
+  const prefersReducedMotion = useReducedMotion()
+  const ref = useRef<HTMLDivElement>(null)
+  const {scrollYProgress} = useScroll({
+    target: ref,
+    // This mean animation starts when the top of the image touches the bottom of the viewport,
+    // and ends when the bottom of the image touches the top of the viewport.
+    offset: ["start end", "end start"],
+  })
+  const yRange = useTransform(scrollYProgress, [0, 1], [offset, -offset])
+  const y = useSpring(yRange, {stiffness: 200, damping: 30})
+
+  const conditionalY = prefersReducedMotion ? 0 : y
+
+  return (
+    <motion.div ref={ref} style={{y: conditionalY}}>
+      {children}
+    </motion.div>
+  )
+}
+
+export default AnimateParallax
