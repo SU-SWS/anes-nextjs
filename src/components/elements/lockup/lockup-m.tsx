16eba90330f312b45e76e2fd9f9a8b199a18f134
Rebecca Hong <34019925+rebeccahongsf@users.noreply.github.com>
2025-10-13 20:37:20 -0700

    ANES3-336 | build out stanford medicine global footer (#16)
    * ANES3-336 | build out stanford medicine global footer
    
    * add stanford medicine affiliate logos
    
    * refine responsive spacing
    
    * adjust spacing around stanford med logo in masthead

--- src/components/elements/lockup/lockup-m.tsx
+++ src/components/elements/lockup/lockup-m.tsx
@@ -7,7 +7,7 @@ const LockupM = ({line1, line2, siteName, logoUrl}: FooterLockupProps) => {
     <div>
       <Link href="/" className="text-black no-underline">
         <div className="flex flex-col gap-12 md:flex-row md:items-center">
-          <div className="border-black pr-12 md:inline-block md:border-r-3">
+          <div className="border-black pr-12 max-sm:m-20 md:inline-block md:border-r-3">
             <LockupLogo logoUrl={logoUrl} siteName={siteName} />
           </div>
           <div className="text-18 md:text-16 lg:text-19 xl:text-23 font-normal">
